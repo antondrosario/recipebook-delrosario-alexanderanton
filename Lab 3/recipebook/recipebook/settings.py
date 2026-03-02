b09a2da8f9b344fc04440ef1585fdb56a65c00b5
antondrosario <antondrosario@gmail.com>
2026-03-02 20:36:03 +0800

    Add EMAIL_BACKEND setting for console email

--- Lab 3/recipebook/recipebook/settings.py
+++ Lab 3/recipebook/recipebook/settings.py
@@ -81,3 +81,7 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = "/recipes/list/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 LOGIN_URL = "/accounts/login/"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
